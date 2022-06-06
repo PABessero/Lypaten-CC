8d78471dcdfab5b9137099dee319a16626bc6ae3
PA Bessero <pierrealain.bessero@gmail.com>
2022-06-06 12:05:37 +0200

    Fixed missing ()

--- MekanismPower/src/main.lua
+++ MekanismPower/src/main.lua
@@ -4,7 +4,7 @@ local mekanism = require('mekanism.mekanismCore')
 local mainMonitor = config.mainMonitor
 
 mainMonitor.monitor = peripheral.wrap(mainMonitor.code)
-mainMonitor.width, mainMonitor.height = mainMonitor.monitor.getSize
+mainMonitor.width, mainMonitor.height = mainMonitor.monitor.getSize()
 mainMonitor.monitor.clear()
 
 
@@ -12,7 +12,7 @@ mainMonitor.monitor.clear()
 local configWindow = window.create(mainMonitor, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
 
 local oldTerm = term.redirect(mainMonitor)
-    paintutils.drawBox(1, 1, config.configWindow.width+2, mainMonitor.height, colors.red)
+    paintutils.drawLine(config.configWindow.width+2, 1, config.configWindow.width+2, mainMonitor.height, colors.red)
 term.redirect(oldTerm)
 
 local mainWindow = window.create(mainMonitor, 16, 1, mainMonitor.width-16, mainMonitor.height)
