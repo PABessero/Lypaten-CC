3137fdd2de48bdeda2b84676d2c023ad1ba1929e
PA Bessero <pierrealain.bessero@gmail.com>
2022-06-06 12:48:00 +0200

    Test

--- MekanismPower/src/main.lua
+++ MekanismPower/src/main.lua
@@ -7,18 +7,18 @@ mainMonitor.monitor = peripheral.wrap(mainMonitor.code)
 mainMonitor.width, mainMonitor.height = mainMonitor.monitor.getSize()
 mainMonitor.monitor.clear()
 
---local monitorWindow = window.create(mainMonitor, 1, 1, mainMonitor.width, mainMonitor.height)
+local monitorWindow = window.create(mainMonitor.monitor, 1, 1, mainMonitor.width, mainMonitor.height)
 
 
---local configWindow = window.create(monitorWindow, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
+local configWindow = window.create(monitorWindow, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
 
---local oldTerm = term.redirect(monitorWindow)
---    paintutils.drawLine(config.configWindow.width+2,1,config.configWindow.width+2,mainMonitor.height,colors.red)
---    monitorWindow.setBackgroundColor(colors.black)
---term.redirect(oldTerm)
+local oldTerm = term.redirect(monitorWindow)
+    paintutils.drawLine(config.configWindow.width+2,1,config.configWindow.width+2,mainMonitor.height,colors.red)
+    monitorWindow.setBackgroundColor(colors.black)
+term.redirect(oldTerm)
 
---local mainWindow = window.create(monitorWindow, 16, 1, mainMonitor.width-16, mainMonitor.height)
---mainWindow.setCursorPos(1, 1)
+local mainWindow = window.create(monitorWindow, 16, 1, mainMonitor.width-16, mainMonitor.height)
+mainWindow.setCursorPos(1, 1)
 
 
-mekanism.setup(config.matrixList, mainMonitor)
+mekanism.setup(config.matrixList, mainMonitor.monitor)
