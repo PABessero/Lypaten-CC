3c56cec43cd65c6ac168128e335fa8bcef0651be
PA Bessero <pierrealain.bessero@gmail.com>
2022-06-06 12:21:16 +0200

    Added main window

--- MekanismPower/src/main.lua
+++ MekanismPower/src/main.lua
@@ -7,15 +7,16 @@ mainMonitor.monitor = peripheral.wrap(mainMonitor.code)
 mainMonitor.width, mainMonitor.height = mainMonitor.monitor.getSize()
 mainMonitor.monitor.clear()
 
+local monitorWindow = window.create(mainMonitor, 1, 1, mainMonitor.width, mainMonitor.height)
 
 
-local configWindow = window.create(mainMonitor, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
+local configWindow = window.create(monitorWindow, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
 
-local oldTerm = term.redirect(mainMonitor)
+local oldTerm = term.redirect(monitorWindow)
     paintutils.drawLine(config.configWindow.width+2, 1, config.configWindow.width+2, mainMonitor.height, colors.red)
 term.redirect(oldTerm)
 
-local mainWindow = window.create(mainMonitor, 16, 1, mainMonitor.width-16, mainMonitor.height)
+local mainWindow = window.create(monitorWindow, 16, 1, mainMonitor.width-16, mainMonitor.height)
 mainWindow.setCursorPos(1, 1)
 
 
