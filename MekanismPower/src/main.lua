bdb516fb712af78efb38ef6d38109420479e72c3
PA Bessero <pierrealain.bessero@gmail.com>
2022-06-06 12:50:27 +0200

    Fixed line position

--- MekanismPower/src/main.lua
+++ MekanismPower/src/main.lua
@@ -13,7 +13,7 @@ local monitorWindow = window.create(mainMonitor.monitor, 1, 1, mainMonitor.width
 local configWindow = window.create(monitorWindow, config.configWindow.width, 1, mainMonitor.width-config.configWindow.width, mainMonitor.height)
 
 local oldTerm = term.redirect(monitorWindow)
-    paintutils.drawLine(config.configWindow.width+2,1,config.configWindow.width+2,mainMonitor.height,colors.red)
+    paintutils.drawLine(config.configWindow.width+1,1,config.configWindow.width+1,mainMonitor.height,colors.red)
     monitorWindow.setBackgroundColor(colors.black)
 term.redirect(oldTerm)
 
